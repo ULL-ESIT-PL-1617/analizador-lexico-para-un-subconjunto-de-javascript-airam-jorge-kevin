beb2fe8195fbab7d151f6aa69e7009bf17d63694
Jorge Sierra <jsierra.siete@gmail.com>
2017-03-31 01:41:32 +0100

    Arreglados scripts y añadido un index al public

--- scripts/deploy-gitbook.js
+++ scripts/deploy-gitbook.js
@@ -5,7 +5,7 @@ var config = require('../package.json');
 var repoUrl = config.repository.url;
 
 console.log("Deploying Gitbook on gh-pages branch");
-ghPages.publish(path.join(__dirname, '/../gh-pages'), {
+ghPages.publish(path.join(__dirname, '/../public'), {
     repo: repoUrl,
     message: 'Auto generated commit for gh-pages.',
     logger: log,
--- scripts/generate-gitbook.js
+++ scripts/generate-gitbook.js
@@ -5,16 +5,16 @@ var config = require('../package.json');
 
 var gBookURL = config.repository.gitbookUrl;
 var docs = path.join(__dirname, '../docs');
-var ghPagesDir = path.join(__dirname, '../gh-pages');
+var ghPagesDir = path.join(__dirname, '../public/gitbook');
 
-if (!fs.existsSync(ghPagesDir)) {
-    console.log("Creating gh-pages dir");
+/*if (!fs.existsSync(ghPagesDir)) {
+    console.log("Creating gitbook dir");
     fs.mkdirSync(ghPagesDir);
-}
+}*/
 
 if (fs.existsSync(docs)) {
     console.log("Accesing docs");
     process.chdir(docs);
     console.log("Creating gitbook");
-    exec('gitbook build . ../gh-pages', {stdio: [0,1,2]});
+    exec('gitbook build . ../public/gitbook', {stdio: [0,1,2]});
 }
