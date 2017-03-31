75a065d07d013199cce8e5c65e70dffc7173e75a
Jorge Sierra <jsierra.siete@gmail.com>
2017-03-31 01:49:04 +0100

    gh-pages

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-
-gulp.task('build', shell.task(['npm run build']));
-gulp.task('deploy', shell.task(['npm run deploy']));
-gulp.task('serve-temporal', shell.task(['npm run serve-temporal']));
-gulp.task('serve', shell.task(['npm run serve']));
